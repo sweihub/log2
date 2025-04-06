3f44bc9cc290cc170ba7a89adb3721b3ebdaaf13
Foxicution <foxicution@gmail.com>
2025-04-06 14:24:43 +0300

    Fix formatting inconsistency
    Now both tee and file formatters have `\n` return.

--- src/lib.rs
+++ src/lib.rs
@@ -337,7 +337,7 @@ impl log::Log for Log2 {
                 let open = "[".truecolor(0x87, 0x87, 0x87);
                 let close = "]".truecolor(0x87, 0x87, 0x87);
                 content = format!(
-                    "{open}{}{close} {open}{}{close} {origin}{}",
+                    "{open}{}{close} {open}{}{close} {origin}{}\n",
                     Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
                     level,
                     record.args()
@@ -530,7 +530,7 @@ fn worker(mut ctx: Context) -> Result<(), std::io::Error> {
                     }
                 }
                 Action::Tee(line) => {
-                    println!("{line}");
+                    print!("{line}");
                 }
                 Action::Flush => {
                     if target.is_some() {
